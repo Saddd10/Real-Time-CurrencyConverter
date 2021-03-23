f8617373c3d315550b3ec3da2a50d5711cd02c73
Saddd10 <73766214+Saddd10@users.noreply.github.com>
2021-03-23 21:41:36 +0700

    Add files via upload

---
+++ MoneyCurrency/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace MoneyCurrency
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Rupiah = new System.Windows.Forms.Label();
+            this.RupiahCurrency = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Display", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(155, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(485, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Money Currency Converter";
+            // 
+            // Rupiah
+            // 
+            this.Rupiah.AutoSize = true;
+            this.Rupiah.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Rupiah.Location = new System.Drawing.Point(228, 215);
+            this.Rupiah.Name = "Rupiah";
+            this.Rupiah.Size = new System.Drawing.Size(40, 22);
+            this.Rupiah.TabIndex = 1;
+            this.Rupiah.Text = "IDR";
+            // 
+            // RupiahCurrency
+            // 
+            this.RupiahCurrency.AccessibleName = "Rupiah";
+            this.RupiahCurrency.Location = new System.Drawing.Point(274, 216);
+            this.RupiahCurrency.Name = "RupiahCurrency";
+            this.RupiahCurrency.Size = new System.Drawing.Size(239, 27);
+            this.RupiahCurrency.TabIndex = 2;
+            this.RupiahCurrency.Tag = "Rupiah";
+            this.RupiahCurrency.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "USD",
+            "JPY"});
+            this.comboBox1.Location = new System.Drawing.Point(564, 215);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(133, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Convert!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(212, 355);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hasil Konversi :";
+            // 
+            // Result
+            // 
+            this.Result.AccessibleName = "Result";
+            this.Result.Location = new System.Drawing.Point(327, 352);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(147, 27);
+            this.Result.TabIndex = 6;
+            this.Result.Tag = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.RupiahCurrency);
+            this.Controls.Add(this.Rupiah);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Rupiah;
+        private System.Windows.Forms.TextBox RupiahCurrency;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Result;
+    }
+}
+
---
+++ MoneyCurrency/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace MoneyCurrency
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double getRate(string fromCurrency, string toCurrency)
+        {
+            var json = "";
+            string rate = "";
+            try
+            {
+                string url = string.Format("https://free.currconv.com/api/v7/convert?q={0}_{1}&compact=ultra&apiKey=1b71d51db3a25220383e", fromCurrency.ToUpper(), toCurrency.ToUpper());
+                string key = string.Format("{0}_{1}", fromCurrency.ToUpper(), toCurrency.ToUpper());
+
+                json = new WebClient().DownloadString(url);
+                dynamic stuff = JsonConvert.DeserializeObject(json);
+                rate = stuff[key];
+            }
+            catch
+            {
+                rate = "0";
+            }
+
+            return double.Parse(rate);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConvertMoney();
+        }
+
+        private void ConvertMoney()
+        {
+            Rupiah.Text = "IDR";
+            if (this.comboBox1.SelectedItem.ToString() == "USD")
+            {
+                double rate = getRate(Rupiah.Text, comboBox1.SelectedItem.ToString());
+                double output = (double.Parse(RupiahCurrency.Text) * rate)/10000000;
+
+                Result.Text = Math.Round(output, 5) + ""; 
+            }
+            else if (this.comboBox1.SelectedItem.ToString() == "JPY")
+            {
+                double rate = getRate(Rupiah.Text, comboBox1.SelectedItem.ToString());
+                double output = (double.Parse(RupiahCurrency.Text) * rate)/1000000;
+
+                Result.Text = Math.Round(output, 5) + ""; 
+            }
+            
+        }
+    }
+}
